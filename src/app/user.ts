49ae954258aae3350458ce2f4871c3759dbbd06b
whitecat1216 <yuukikotani1216@gmail.com>
2025-12-19 17:56:37 +0900

    [add]ルーティングの概要まで終了

--- src/app/user.ts
+++ src/app/user.ts
@@ -1,11 +1,27 @@
-import {Component, input} from '@angular/core';
+import { Component } from '@angular/core';
+import { NgOptimizedImage } from '@angular/common';
 
 @Component({
   selector: 'app-user',
+  standalone: true,
+  imports: [NgOptimizedImage],
   template: `
-    <p>The user's name is {{ name() }}</p>
+    <p>Username: {{ username }}</p>
+    <p>Preferred Framework</p>
+    <ul>
+      <li>
+        Static Image:
+        <img ngSrc="/assets/logo.svg" alt="Angular logo" width="32" height="32">
+      </li>
+      <li>
+        Dynamic Image:
+        <img [ngSrc]="logoUrl" [alt]="logoAlt" width="32" height="32"/>
+      </li>
+    </ul>
   `,
 })
 export class User {
-  readonly name = input<string>();
+  logoUrl = '/assets/logo.svg';
+  logoAlt = 'Angular logo';
+  username = 'youngTech';
 }
